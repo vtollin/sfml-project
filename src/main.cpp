32d5977b3a99759ee7623f9a3ce8bd3d87776614
Valentino <vtollin@umich.edu>
2024-05-07 15:45:03 -0400

    Played around with drawing a circle

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "Point Charge", sf::Style::Default};
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -14,8 +14,14 @@ int main()
                 window.close();
             }
         }
-
-        window.clear();
+        sf::CircleShape pC(50.f);
+        pC.setOrigin(pC.getRadius(), pC.getRadius());
+        pC.setFillColor(sf::Color(97, 141, 212));
+        sf::Vector2f winSize(window.getSize());
+        sf::Vector2f center(winSize/2.f);
+        pC.setPosition(center);
+        window.clear(sf::Color::White);
+        window.draw(pC);
         window.display();
     }
 }
